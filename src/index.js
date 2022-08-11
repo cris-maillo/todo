1b1256f3d7c289f04f9a9ef2ef8b7bebee650e95
cris-maillo <cristinamaillo.r@gmail.com>
2022-08-11 23:35:05 +0100

    set local storage

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@ import {deleteItem} from "./deleteItem.js";
 const form  = document.getElementById('addform');
 const projectForm  = document.getElementById('addprojectform');
 
+var projects;
+var toDoList;
+let chosenProject;
 
 
 (function(){
-  var projects;
-  var toDoList;
-
-  if (localStorage.getItem("localProjects") !== null || localStorage.getItem("localDos") !== null) {
-    console.log("local")
-    projects = JSON.parse(localStorage.getItem("localProjects"))
-    toDoList = JSON.parse(localStorage.getItem("localDos"))
-  }
-  else {
-    toDoList = [{title: 'Look Pretty', dueDate: '2022-07-21', completed: false, assignedProject: "Inbox"}, {title: 'Finish To Do App', dueDate: '2022-07-21', completed: false, assignedProject: "Coding"}, {title: 'Have Fun', dueDate: '2022-07-21', completed: false, assignedProject: "Inbox"}];
+  if (localStorage.getItem("localProjects") == null || localStorage.getItem("localDos") == null) {
+    toDoList = [{title: 'Look Pretty', dueDate: '2022-07-21', completed: false, assignedProject: "Inbox", dueToday: false}, {title: 'Finish To Do App', dueDate: '2022-07-21', completed: false, assignedProject: "Coding", dueToday: false}, {title: 'Have Fun', dueDate: '2022-07-21', completed: false, assignedProject: "Inbox", dueToday: false}];
     projects = ["Inbox", "Due Today", "Coding"]
+  }else {
+    toDoList = JSON.parse(localStorage.getItem("localDos"))
+    projects = JSON.parse(localStorage.getItem("localProjects"))
   }
 
+  chosenProject = projects[0]
   projectForm.addEventListener('submit', (event) => {
     event.preventDefault();
     let projectName = projectForm.elements["project"].value
@@ -29,7 +28,25 @@ const projectForm  = document.getElementById('addprojectform');
     displayProjects(projects);
   });
 
-  let chosenProject = projects[0]
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let title = form.elements["title"].value
+    let dueDate = form.elements["dueDate"].value
+
+    const result = parseISO(dueDate)
+    let assignedProject = form.elements["project"].value;
+    let dueToday;
+    if (isToday(result)){
+      dueToday = true;
+    }else{
+      dueToday = false;
+    }
+    var ToDo1 = new ToDo(title, dueDate, false, assignedProject, dueToday)
+    toDoList.push(ToDo1);
+    chosenProject = assignedProject;
+    displayList(chosenProject, toDoList);
+
+  });
   displayList(chosenProject, toDoList);
   displayProjects(projects);
 })();
@@ -37,22 +54,8 @@ const projectForm  = document.getElementById('addprojectform');
 function displayList(chosenProject, toDoList){
   localStorage.setItem("localDos", JSON.stringify(toDoList))
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let title = form.elements["title"].value
-    let dueDate = form.elements["dueDate"].value
+  console.log(localStorage.getItem("localDos"))
 
-    const result = parseISO(dueDate)
-    let assignedProject;
-    if (isToday(result)){
-      assignedProject = "Due Today";
-    }else{
-      assignedProject = form.elements["project"].value;
-    }
-    var ToDo1 = new ToDo(title, dueDate, false, assignedProject)
-    toDoList.push(ToDo1);
-    displayList(chosenProject);
-  });
 
   const listContainer = document.getElementById("todos");
   const listHeading = document.getElementById("listHeading");
@@ -69,9 +72,9 @@ function displayList(chosenProject, toDoList){
     listContainer.removeChild(listContainer.lastChild);
   }
 
-
+  
   for (let i = 0; i < toDoList.length; i++){
-    if (toDoList[i].assignedProject == chosenProject){
+    if ((chosenProject == "Inbox" || toDoList[i].assignedProject == chosenProject) || (chosenProject == "Due Today" && toDoList[i].dueToday == true)){
       let itemContainer = document.createElement("div");
       itemContainer.className = "todo";
       
@@ -81,21 +84,21 @@ function displayList(chosenProject, toDoList){
       
       let completeStatus = toDoList[i].completed;
       item.addEventListener("click", function() { markComplete(completeStatus, i, toDoList, chosenProject); } , false)
-
+  
       if(toDoList[i].completed === true){
         item.classList = "completed";
       }
-
+  
       let itemDate = document.createElement("div");
       itemDate.className = "dueDate";
       itemDate.innerHTML = toDoList[i].dueDate;
-
+  
       let itemDelete = document.createElement("img");
       itemDelete.src = "img/trash-can.png";
       itemDelete.className = "itemDelete";
       itemDelete.addEventListener("click", function() { deleteItem(i, toDoList, chosenProject); } , false)
       itemDelete.width = 25;
-
+  
       let itemLeft = document.createElement("div");
       itemLeft.className = "itemLeft";
 
@@ -106,13 +109,10 @@ function displayList(chosenProject, toDoList){
       listContainer.appendChild(itemContainer);
     }
   }
-    
 }
 
 function displayProjects(projects){
-
   localStorage.setItem("localProjects", JSON.stringify(projects))
-
 
   const projectContainer = document.getElementById("projectlist");
   const select = document.getElementById("projectSelect");
@@ -129,7 +129,7 @@ function displayProjects(projects){
     let projectName = document.createElement("h1");
     projectName.innerHTML = projects[i];
     let chosenProject = projects[i];
-    projectName.addEventListener("click", function() { displayList(chosenProject); } , false)
+    projectName.addEventListener("click", function() { displayList(chosenProject, toDoList); } , false)
     projectContainer.appendChild(projectName);
 
     if(projects[i] != "Due Today"){
--- src/newToDo.js
+++ src/newToDo.js
@@ -1,10 +1,11 @@
 class ToDo{
-    constructor(title, dueDate, completed, assignedProject){
+    constructor(title, dueDate, completed, assignedProject, dueToday){
         this.title = title;
         // this.id = id;
         this.dueDate = dueDate;
         this.completed = completed;
         this.assignedProject = assignedProject;
+        this.dueToday = dueToday;
     }
   }
 
